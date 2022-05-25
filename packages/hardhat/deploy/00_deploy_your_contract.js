9b0c75901daf18b2a1dd5ec9aa4a7c770a9880d7
Ewan Humbert <xeway@protonmail.com>
2022-05-26 00:44:53 +0200

    Automated abi exporting

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -1,5 +1,18 @@
 // deploy/00_deploy_your_contract.js
 
+const fs = require("fs");
+const { abi } = require("../artifacts/contracts/Spouf.sol/Spouf.json");
+
+let obj = {
+  42: {
+    kovan: {
+      name: "kovan",
+      chainId: "42",
+      contracts: {}
+    }
+  }
+}
+
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
@@ -13,6 +26,21 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   await proxyContract.deployed();
 
   console.log("Proxy's address: ", proxyContract.address);
+
+  obj["42"].kovan.contracts = {...obj["42"].kovan.contracts, Spouf: {
+    address: proxyContract.address,
+    abi: abi
+  }};
+
+  console.log(obj);
+
+  fs.writeFileSync("../react-app/src/contracts/hardhat_contracts.json", JSON.stringify(obj, null, 3), (err) => {
+    if (err) {
+      console.log("Error to write file", err);
+      return;
+    }
+    console.log("File created");
+  });
 
   // Verify from the command line by running `yarn verify`
 
