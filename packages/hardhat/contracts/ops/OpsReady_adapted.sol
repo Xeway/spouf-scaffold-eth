eb6791f3fa131e7cc4020a094055e118a0b13a83
Ewan Humbert <xeway@protonmail.com>
2022-06-04 13:22:32 +0200

    Deployed contract and fixed upgradeable bug

--- packages/hardhat/contracts/ops/OpsReady_adapted.sol
+++ packages/hardhat/contracts/ops/OpsReady_adapted.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.0;
 // just adapted it because Spouf contract doesn't support a constructor because it's a proxy
 
 import {
-    SafeERC20,
-    IERC20
-} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+    SafeERC20Upgradeable,
+    IERC20Upgradeable
+} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 interface IOps {
     function gelato() external view returns (address payable);
@@ -35,7 +35,7 @@ abstract contract OpsReady {
             (bool success, ) = gelato.call{value: _amount}("");
             require(success, "_transfer: ETH transfer failed");
         } else {
-            SafeERC20.safeTransfer(IERC20(_paymentToken), gelato, _amount);
+            SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(_paymentToken), gelato, _amount);
         }
     }
 }
